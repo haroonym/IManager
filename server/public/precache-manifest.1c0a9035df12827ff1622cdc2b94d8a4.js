25c230e018f65d2a025ee2111818a95a31928a7f
haroonym <71009111+haroonym@users.noreply.github.com>
2022-01-27 12:49:36 +0100

    dist in server geladen

---
+++ server/public/precache-manifest.1c0a9035df12827ff1622cdc2b94d8a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "326b0d3bad9b87f2eb4c",
+    "url": "/css/chunk-vendors.8e78e50d.css"
+  },
+  {
+    "revision": "f42c8dcfec8886d94aa5dd6b10e6d354",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffc8f2d83a16c46df82a",
+    "url": "/js/about.fa57e3b5.js"
+  },
+  {
+    "revision": "72c6ef686c62a9d38f16",
+    "url": "/js/app.29e04121.js"
+  },
+  {
+    "revision": "326b0d3bad9b87f2eb4c",
+    "url": "/js/chunk-vendors.67a0958d.js"
+  },
+  {
+    "revision": "75087e4c01bb34f6be51dd027961985b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
