ef8e4e280a464348ff9a4447b0ef0e4d29c44df6
haroonym <71009111+haroonym@users.noreply.github.com>
2022-01-27 12:45:19 +0100

    App Frame fertig - WorkBox muss noch installiert werden

---
+++ client/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-app-bar color="white accent-4" dark app>
+        <v-toolbar-title>
+          <v-img src="./assets/imanager.png" width="50px" class="mr-3"></v-img>
+        </v-toolbar-title>
+        <h3 class="black--text">iManager</h3>
+      </v-app-bar>
+      <v-container> <router-view /></v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+// import axios from 'axios';
+export default {
+  name: 'App',
+  // serverAddress: process.env.VUE_APP_SERVER,
+  data: () => ({
+    //
+  }),
+  methods: {
+    // async getItems() {
+    //   try {
+    //     const { data } = await axios({
+    //       url: this.serverAddress + '/items',
+    //       method: 'GET',
+    //     });
+    //     this.items = data;
+    //   } catch (error) {
+    //     console.error(error);
+    //   }
+    // },
+  },
+};
+</script>
---
+++ client/src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <p class="text-h5 text-center mt-2">Yuhuuu Geschafft!!!</p>
+    <h3>Willkommen bei der Service Worker Untersuchung!</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+};
+</script>
