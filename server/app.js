3eb0a533eadba87221223df75f649714701b4333
haroonym <71009111+haroonym@users.noreply.github.com>
2022-02-18 13:12:52 +0100

    make it better

--- server/app.js
+++ server/app.js
@@ -20,7 +20,6 @@ app.use(express.static(path.join(__dirname, '/public')));
 
 app.use(express.json());
 
-// app.get('/test', (req, res) => res.send('Yeah, you reached the test route!'));
 app.use('/', routes);
 
 const PORT = process.env.PORT ?? 5000;
